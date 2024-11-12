8c9b9d59d36cd75ac6eab859491133a1099bc300
DieseVevi-35 <148536318+DieseVevi-35@users.noreply.github.com>
2024-11-12 17:00:25 +0100

    register - css attributes (textfield color change)

--- register.js
+++ register.js
@@ -1,5 +1,6 @@
 function checkUsername(callback) {
     const username = document.getElementById("username").value;
+    const usernameColor = document.getElementById("username");
 
     if (username.length < 3) {
         alert("Username must be at least 3 characters long.");
@@ -12,9 +13,13 @@ function checkUsername(callback) {
             if (xmlhttp.status === 204) {
                 console.log("Exists");
                 alert("Username already exists. Please choose another one.");
+                usernameColor.style.border = "2px solid rgb(122, 5, 3)";
+                usernameColor.style.backgroundColor = "rgb(201, 8 , 5)";
                 return;
             } else if (xmlhttp.status === 404) {
                 console.log("Does not exist");
+                usernameColor.style.border = "2px solid rgb(116, 185, 105)";
+                usernameColor.style.backgroundColor = "rgb(224, 242, 206)";
                 callback();
             }
         }
@@ -28,14 +33,25 @@ function checkUsername(callback) {
 function checkPassword() {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm_password").value;
+    const passwordColor = document.getElementById("password");
+    const confirmPasswordColor = document.getElementById("confirm_password");
 
     if (password.length < 8) {
         alert("Password must be at least 8 characters long.");
+        passwordColor.style.border = "2px solid rgb(122, 5, 3)";
+        passwordColor.style.backgroundColor = "rgb(201, 8 , 5)";
         return false;
+    } else {
+        passwordColor.style.border = "2px solid rgb(116, 185, 105)";
+        passwordColor.style.backgroundColor = "rgb(224, 242, 206)";
     }
 
     if (password !== confirmPassword) {
         alert("Passwords do not match.");
+        passwordColor.style.border = "2px solid rgb(122, 5, 3)";
+        passwordColor.style.backgroundColor = "rgb(201, 8 , 5)";
+        confirmPasswordColor.style.border = "2px solid rgb(122, 5, 3)";
+        confirmPasswordColor.style.backgroundColor = "rgb(201, 8 , 5)";
         return false;
     }
 
@@ -50,11 +66,3 @@ function validateForm(event) {
         }
     });
 }
-
-/*
-ToDo Verena:
-- rote Umrandungen --> css!
-- Klärung Case-Sensitivity bei Username Prüfung
-- method="get" da "post" nicht funktioniert --> mit php umsetzen?
-- bei Password/Confirm Password: required hinzufügen? (da man sonst immer erst alles ausfüllen muss, bevor prüfung erfolgt)
-*/
