38591f46712a09a067a17379fa1613d259403f4b
niroeth <164351407+niroet@users.noreply.github.com>
2024-12-15 13:26:06 +0000

    login etc

--- login.php
+++ login.php
@@ -10,6 +10,36 @@
     <?php ?>
     <?php
     include 'start.php';
+    require 'start.php';
+    if (isset($user)){
+        header("Location friends.php");
+    }
+    include 'utils/BackendService.php';
+
+    //Login handling
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        //retrieve input here
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //create a new BackendService Object
+        $backendService = new \Utils\BackendService('https://online-lectures-cs.thi.de/chat','1960b373-3673-45f0-8509-c3d7d901a332');
+
+        //call login method
+        $loginSuccess = $backendService->login($username,$password);
+
+        //handling of result
+        if ($loginSuccess){
+            //redirect to freinds by creating header
+            header("Location: friends.php");
+            exit();
+        } else {
+            //Loginfail
+            $errorMessage="Invalid username or password.";
+        }
+    }
+
+    
     ?>
 </head>
 
@@ -27,6 +57,11 @@
             <input type='password' id='password' placeholder='Password' name='password'>
             <br>
         </fieldset>
+
+<!--hier für das Feedback falls falsch eingegeben -->
+        <?php if (isset($errorMessage)): ?>
+            <p style="color: red;"><?php echo $errorMessage; ?></p>
+        <?php endif; ?>
 
         <!--damit die buttons später noch nebeneinander sind-->
         <div class="button-container">
--- logout.php
+++ logout.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log Out</title>
     <link rel="stylesheet" href="styles.css">
-</head>
+    <?php include 'start.php' 
+    session_unset(); //Gott take a look at the workings when all files are complete 
+    session_destroy();
+    ?>
+</head> 
 <body>
     <img src="images/catBye.gif" alt='logout-symbol' width='100' height='100'>
 
