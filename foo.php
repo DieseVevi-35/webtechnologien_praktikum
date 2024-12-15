13aad285d16742716ea0789bd60a5d9a698822dc
niroeth <164351407+niroet@users.noreply.github.com>
2024-12-15 14:11:55 +0000

    instanzierung geändert

--- foo.php
+++ foo.php
@@ -1,17 +1,22 @@
+<?php 
+    require("start.php");
+    $user = new Model\User("Test");
+    $json = json_encode($user);
+    echo $json . "<br>";
+    $jsonObject = json_decode($json);
+    $newUser = Model\User::fromJson($jsonObject);
+    
+    ?>
+
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php
-    require("start.php");
-    $user = new Model\User("Test");
-    $json = json_encode($user);
-    ?>
-
 </head>
 <body>
-    <?php echo $json; ?>
+    <?php var_dump($newUser); ?>
 </body>
 </html>
