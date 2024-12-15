ead617e0a793c1f3b96d3bcfd2565b6d21bef666
Anton <ton-An@outlook.de>
2024-12-15 14:04:56 +0100

    Added BackendService init to start.php

--- start.php
+++ start.php
@@ -8,4 +8,5 @@ session_start();
 define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat/');
 define('CHAT_SERVER_ID', '1960b373-3673-45f0-8509-c3d7d901a332'); #
 
+$service = new Utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
 ?>
